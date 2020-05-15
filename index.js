44caca74cbc5f25eb2294476ec90c11feb80db46
omu-0421 <kingyohanabi0217@me.com>
2020-05-15 09:30:50 +0900

    git commit -m "js修正"

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 console.log('omuomu');
 
 
-
 window.onload = () => {
-
-    Array.from(document.getElementsByName('grid-item')).forEach(element => {
-        element.addEventListener('click',(e) => {
-            e.target.dataset.color = 'white'
-        })
-    })
+    document.getElementsByClassName('grid-item')
 
 
 
-    document.getElementById('c1').addEventListener('click', (e) => {
-        e.target.dataset.color = 'white'
+    document.getElementById('c1').addEventListener('click', (e) =>{
+        e.target.detaset.color = 'white'
     })
 
 }
